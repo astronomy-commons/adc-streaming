6c6dd5072f477c35d49f4a1c468260761b9619b5
Mario Juric <mjuric@astro.washington.edu>
2020-02-14 16:19:01 -0800

    run.py: simple test util
    TODO: replace this with a more serious command line entrypoint at a latter
    time

---
+++ run.py
@@ -0,0 +1,10 @@
+import genesis.streaming as gs
+
+message = { 'hello': 'world' }
+
+with gs.open("kafka://localhost/test1", "rw", start_at="earliest", format="json") as stream:
+	stream.write(message)
+
+#with gs.open("kafka://localhost/test1", "r", start_at="earliest", format="json") as stream:
+	for _, rec in stream(timeout=10):
+		print(rec)
