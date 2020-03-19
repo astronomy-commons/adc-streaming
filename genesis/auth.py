a7226cd274f5f63f3d270ff789a4ed9d1209088d
Patrick Godwin <patrick.godwin@psu.edu>
2020-03-19 16:11:10 -0400

    auth.py: add SASLMethod to replace choosing SASL methods in auth

--- genesis/auth.py
+++ genesis/auth.py
@@ -1,6 +1,20 @@
 #!/usr/bin/env python
 
+from enum import Enum
+
 import certifi
+
+
+class SASLMethod(Enum):
+    """SASL method to use for authentication.
+    """
+
+    PLAIN = 1
+    SCRAM_SHA_256 = 2
+    SCRAM_SHA_512 = 3
+
+    def __str__(self):
+        return self.name.replace("_", "-")
 
 
 class SASLAuth(object):
@@ -16,20 +30,17 @@ class SASLAuth(object):
         Password to authenticate with.
     ssl : `bool`, optional
         Whether to enable SSL (enabled by default).
-    method : `str`, optional
-        The SASL method to authenticate, default = PLAIN.
-        Options are [PLAIN, SCRAM-SHA-256, SCRAM-SHA-512].
+    method : `SASLMethod`, optional
+        The SASL method to authenticate, default = SASLMethod.PLAIN.
+        See valid SASL methods in SASLMethod.
     ssl_ca_location : `str`, optional
         If using SSL via a self-signed cert, a path/location
         to the certificate.
 
     """
 
-    _methods = ["PLAIN", "SCRAM-SHA-256", "SCRAM-SHA-512"]
-
-    def __init__(self, user, password, ssl=True, method="PLAIN", **kwargs):
-        self._method = method.upper()
-        assert self._method in self._methods
+    def __init__(self, user, password, ssl=True, method=SASLMethod.PLAIN, **kwargs):
+        self._method = method
 
         # set up SSL options
         if ssl:
@@ -46,7 +57,7 @@ class SASLAuth(object):
             self._config = {"security.protocol": "SASL_PLAINTEXT"}
 
         # set up SASL options
-        self._config["sasl.mechanism"] = self._method
+        self._config["sasl.mechanism"] = str(self._method)
         self._config["sasl.username"] = user
         self._config["sasl.password"] = password
 
